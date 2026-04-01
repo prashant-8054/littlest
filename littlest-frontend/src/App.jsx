c977e5ada5a3da25c79404374a6901093b70405a
Prashant Kumar <prax.pks@gmail.com>
2026-04-01 14:21:47 +0530

    wrote a bit of const's

--- littlest-frontend/src/App.jsx
+++ littlest-frontend/src/App.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react"
+
 export default function App() {
 
+  const [beats, setBeats] = useState([]);
+  const [myBeats, setMyBeats] = useState([]);
+  const [following, setFollowing] = useState([]);
+  const [customUsers, setCustomUsers] = useState([]);
+  const [activeTab, setActiveTab] = useState("feed");
+  const [feedType, setFeedType] = useState("all");
+  const [toast, setToast] = useState(null);
+
+  const handlePost = ({ word, emoji, note, font, color, size}) => {
+    const nb = {
+      id: Date.now(),
+      word,
+      emoji,
+      font,
+      color,
+      size,
+      note,
+      user: "you",
+      time: "just now",
+      reactions: { vibe: 0, empathize: 0, uplift: 0},
+      userReacted: { vibe: false, empathize: false, uplift: false },
+    };
+    setBeats((prev) => [nb, ...prev]);
+    setMyBeats((prev) => [...prev, { ...nb }]);
+    setToast(`"${word}" ${emoji || ""} shared ✦`);
+    setActiveTab("feed");
+  };
+
+  const handleReact = (beatId, key) => {
+    setBeats((prev))
+  }
+
+
   return (
-    <h1>I'm starting to build my minimalistic social media app</h1>
-  )
+    <h1></h1>
+  );
 }
